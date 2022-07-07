32b805c18c2df9e0c0dc46c2e88011571dd5fbd7
Samuel Greco <s.greco@cybeur.eu>
2022-07-07 19:50:29 +0200

    Update

--- nodejs/app/server.js
+++ nodejs/app/server.js
@@ -30,10 +30,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static("public"))
 
 app.get("/", async (req, res) => {
-    let neigh = await db.collection("neighborhoods").find().limit(10).toArray();
+    let neigh = await db.collection("neighborhoods").find().toArray();
+    let restaurants = await db.collection("restaurants").find().limit(10).toArray();
+    console.log(restaurants)
     res.render("index", {
         pageTitle: "Demo",
-        neighborhoods: neigh
+        neighborhoods: neigh,
+        restaurants: restaurants
     })
 });
 
